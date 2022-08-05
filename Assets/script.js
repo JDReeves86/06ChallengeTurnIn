eaa2337b63e62e40f11d614b867c7b73cec785df
JDReeves86 <Reeves.Jacobd@gmail.com>
2022-08-04 20:37:01 -0700

    still fetching

--- Assets/script.js
+++ Assets/script.js
@@ -1,10 +1,14 @@
 const weatherContainer = document.getElementById('container')
+const requestUrl = 'https://api.weather.gov/alerts' // NWS
 
-
-function getWeather() {
-    const requestUrl = 'https://api.weather.gov'
-    fetch(requestUrl)
+function getWeather(request) {
+    fetch(request)
         .then(function(response) {
-            return response.JSON
+            return response.json()
         })
+        .then(function(data) {
+            console.log(data)
+            }) 
 }
+
+getWeather(requestUrl)
